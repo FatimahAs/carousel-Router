08e800acfe868eed5e65ae13adbcbf4bc3d6dea4
FATIMAH ASIRI <fatimahasiri@FATIMAHs-MacBook-Pro-2.local>
2025-05-22 09:08:25 +0300

    edit footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -14,10 +14,10 @@ export default function Footer() {
     
 
       <div className="flex gap-4 flex-wrap justify-center">
-     <Link to="./"><img src="https://images.pexels.com/photos/32109973/pexels-photo-32109973/free-photo-of-scenic-geothermal-landscape-in-iceland.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load" alt='desert' className="w-full h-full object-cover w-20 h-20 md:w-24 md:h-24 rounded-full overflow-hidden border-4 cursor-pointer transition-transform" /></Link>
-     <Link to='./forest'><img src="https://images.pexels.com/photos/33109/fall-autumn-red-season.jpg?auto=compress&cs=tinysrgb&w=800" alt='Forest' className="w-full h-full object-cover w-20 h-20 md:w-24 md:h-24 rounded-full overflow-hidden border-4 cursor-pointer transition-transform" /></Link>
-     <Link to='./city'><img src='https://images.pexels.com/photos/169647/pexels-photo-169647.jpeg?auto=compress&cs=tinysrgb&w=800' alt='City' className="w-full h-full object-cover w-20 h-20 md:w-24 md:h-24 rounded-full overflow-hidden border-4 cursor-pointer transition-transform" /></Link>
-     <Link to='./ocean'><img src='https://images.pexels.com/photos/847393/pexels-photo-847393.jpeg?auto=compress&cs=tinysrgb&w=800' alt='Ocean' className="w-full h-full object-cover w-20 h-20 md:w-24 md:h-24 rounded-full overflow-hidden border-4 cursor-pointer transition-transform" /></Link>
+     <Link to="./"><img src="https://images.pexels.com/photos/32109973/pexels-photo-32109973/free-photo-of-scenic-geothermal-landscape-in-iceland.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load" alt='desert' className="rounded-full overflow-hidden border-4 cursor-pointer transition-transform  object-cover w-20 h-20" /></Link>
+     <Link to='./forest'><img src="https://images.pexels.com/photos/33109/fall-autumn-red-season.jpg?auto=compress&cs=tinysrgb&w=800" alt='Forest' className="rounded-full overflow-hidden border-4 cursor-pointer transition-transform  object-cover w-20 h-20  " /></Link>
+     <Link to='./city'><img src='https://images.pexels.com/photos/169647/pexels-photo-169647.jpeg?auto=compress&cs=tinysrgb&w=800' alt='City' className="rounded-full overflow-hidden border-4 cursor-pointer transition-transform  object-cover w-20 h-20 " /></Link>
+     <Link to='./ocean'><img src='https://images.pexels.com/photos/847393/pexels-photo-847393.jpeg?auto=compress&cs=tinysrgb&w=800' alt='Ocean' className="rounded-full overflow-hidden border-4 cursor-pointer transition-transform  object-cover w-20 h-20" /></Link>
           </div>
        
       </div>
